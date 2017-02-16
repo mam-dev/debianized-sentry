1f8e7d710e86a6b143a626e4ae5a9eb0e8bdcf0f
jhermann <jh@web.de>
2017-02-17 00:56:19 +0100

    log cron output to /var/log/sentry/cleanup.log

--- debian/sentry.cron.d
+++ debian/sentry.cron.d
@@ -1,4 +1,4 @@
 # Sentry data house-keeping
 
 # m h   dom mon dow   user        command
-7 0     *   *   *     sentry      . /etc/default/sentry && /usr/bin/sentry --config="$SENTRY_CONF" cleanup --days="$SENTRY_CLEANUP_DAYS"
+7 0     *   *   *     sentry      . /etc/default/sentry && /usr/bin/sentry >/var/log/sentry/cleanup.log 2>&1 --config="$SENTRY_CONF" cleanup --days="$SENTRY_CLEANUP_DAYS"
