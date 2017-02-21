9ba9002c9bd57532833859252980e80698507c1b
jhermann <jh@web.de>
2017-02-21 16:19:45 +0100

    set cron environment

--- debian/sentry.cron.d
+++ debian/sentry.cron.d
@@ -1,4 +1,8 @@
 # Sentry data house-keeping
 
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+MAILTO=root@localhost
+
 # m h   dom mon dow   user        command
 7 0     *   *   *     sentry      . /etc/default/sentry && /usr/bin/sentry >/var/log/sentry/cleanup.log 2>&1 --config="$SENTRY_CONF" cleanup --days="$SENTRY_CLEANUP_DAYS"
