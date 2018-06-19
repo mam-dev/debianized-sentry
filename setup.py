f262a005625384d50f001a0a79984623ec818183
jhermann <jh@web.de>
2018-06-19 13:35:35 +0200

    setup: better handle '~~rc' package versions

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 # mkvenv: no-deps
 """ Debian packaging for Sentry, a modern realtime error logging and aggregation platform.
 
-    | Copyright ©  2017 1&1 Group
+    | Copyright ©  2017, 2018 1&1 Group
     | BSD 3-clause license, see LICENSE for details.
     |
     | Copyright (c) 2017 Sentry (https://sentry.io) and individual contributors.
@@ -39,7 +39,7 @@ except ImportError as exc:
 
 # get external project data (and map Debian version semantics to PEP440)
 pkg_version = subprocess.check_output("parsechangelog | grep ^Version:", shell=True)
-upstream_version, maintainer_version = pkg_version.split()[1].rsplit('~', 1)[0].split('-', 1)
+upstream_version, maintainer_version = pkg_version.split()[1].split('~', 1)[0].split('-', 1)
 maintainer_version = maintainer_version.replace('~~rc', 'rc').replace('~~dev', '.dev')
 pypi_version = upstream_version + '.' + maintainer_version
 
@@ -51,7 +51,13 @@ maintainer, email = re.match(r'(.+) <([^>]+)>', deb_source['Maintainer']).groups
 desc, long_desc = deb_binary['Description'].split('.', 1)
 desc, pypi_desc = __doc__.split('\n', 1)
 long_desc = textwrap.dedent(pypi_desc) + textwrap.dedent(long_desc).replace('\n.\n', '\n\n')
+dev_status = 'Development Status :: 5 - Production/Stable'
 
+if '~~rc' in pkg_version:
+    rc_tag = pkg_version.split('~')[-1].split('~')[0].split('-')[0]
+    upstream_version += rc_tag
+    pypi_version += rc_tag
+    dev_status = 'Development Status :: 4 - Beta'
 
 # build setuptools metadata
 project = dict(
@@ -65,7 +71,7 @@ project = dict(
     url=deb_source['Homepage'],
     classifiers=[
         # Details at http://pypi.python.org/pypi?:action=list_classifiers
-        'Development Status :: 5 - Production/Stable',
+        dev_status,
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
